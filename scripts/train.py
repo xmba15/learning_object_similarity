a2883a555dc68a31de019de88cee056cbba4f26c
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2017-12-22 14:44:20 +0900

    mod train code

--- scripts/train.py
+++ scripts/train.py
@@ -4,19 +4,23 @@
 import os
 from training_pairs_generator import SiameseNetworkDataset
 from siamese_network import SiameseNetwork, ContrastiveLoss
+import torch
 from torch.autograd import Variable
 from torch.utils.data import DataLoader,Dataset
 from torch import optim
 
+from utils import imshow, show_plot
+
 directory_root = os.path.dirname(os.path.realpath(__file__)) + "/../"
 data_path = directory_root + "/data/raw_training_data/"
+model_path = directory_root + "/models/"
 
 class Config():
 
     training_dir = data_path
     testing_dir = None
-    train_batch_size = 64
-    train_number_epochs = 1
+    train_batch_size = 500
+    train_number_epochs = 10
 
 if __name__=="__main__":
 
@@ -53,4 +57,5 @@ if __name__=="__main__":
                 counter.append(iteration_number)
                 loss_history.append(loss_contrastive.data[0])
 
-    show_plot(counter,loss_history)    
+    torch.save(net.state_dict(), model_path + "/siamese.pth")
+    # show_plot(counter,loss_history)

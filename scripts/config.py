e1efcd22da8be1025ef4a5a40accf384daa66272
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2017-12-23 18:09:26 +0900

    mod config

--- scripts/config.py
+++ scripts/config.py
@@ -6,6 +6,7 @@ directory_root = os.path.dirname(os.path.realpath(__file__)) + "/../"
 data_path = directory_root + "/data/raw_training_data/"
 test_data_path = directory_root + "/data/raw_testing_data/"
 model_path = directory_root + "/models/"
+image_path = directory_root + "/images/"
 
 import torchvision.transforms as transforms
 
@@ -27,6 +28,7 @@ class Config():
     training_dir = data_path
     testing_dir = test_data_path
     model_dir = model_path
+    image_dir = image_path
     train_batch_size = 50
     train_number_epochs = 100
     transforms = _preprocess

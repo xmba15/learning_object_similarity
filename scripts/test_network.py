42d69bc1de34ee2cb6a5f09193ccb564d35bf885
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2018-02-19 18:03:24 +0900

    mod test network

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -21,6 +21,8 @@ from new_augmentation import SquareZeroPadding
 from config import Config
 from utils import imshow, show_plot
 
+from utils import imshow
+import argparse
 
 import torchvision.transforms as transforms
 normalize = transforms.Normalize(
@@ -42,8 +44,30 @@ def load_image(img_path):
 
     return img.unsqueeze(0)
 
+_preprocess_2 = transforms.Compose([
+    SquareZeroPadding(),
+    transforms.Resize((224, 224), 2),
+    transforms.ToTensor(),
+])
+
+def load_image_2(img_path):
+
+    img = Image.open(img_path)
+    img = _preprocess_2(img)
+
+    return img.unsqueeze(0)
+    
 if __name__=="__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--visualization",
+        help = "show visualization",
+        type = bool,
+        default = False
+    )
+    args = parser.parse_args()
+    
     tnet = TripletNetwork().cuda()
     tnet = torch.nn.DataParallel(tnet)
     tnet.eval()
@@ -52,8 +76,8 @@ if __name__=="__main__":
     img1_path = Config.image_dir + "/noodle.jpg"
     img2_path = Config.image_dir + "/noodle3.jpg"
     img3_path = Config.image_dir + "/noodle4.jpg"    
-    p_path = Config.image_dir + "/noodle2.jpg"
-    n_path = Config.image_dir + "/fast_mask_roi_8.jpg"
+    p_path = Config.image_dir + "/noodle5.jpg"
+    n_path = Config.image_dir + "/yel_noodle.jpg"
 
     img1 = load_image(img1_path)
     img2 = load_image(img2_path)
@@ -63,5 +87,17 @@ if __name__=="__main__":
     
     output1, output2, output3 = tnet(Variable(img1).cuda(), Variable(img2).cuda(), Variable(img3).cuda(), Variable(p).cuda(), Variable(n).cuda())
 
-    print F.pairwise_distance(output1, output2)    
-    print F.pairwise_distance(output1, output3)    
+    p_distance =  F.pairwise_distance(output1, output2)    
+    n_distance = F.pairwise_distance(output1, output3)    
+
+    print p_distance
+    print n_distance
+
+    if (args.visualization):
+        _img1 = load_image_2(img1_path)
+        _img2 = load_image_2(img2_path)
+        _img3 = load_image_2(img3_path)
+        _p = load_image_2(p_path)
+        _n = load_image_2(n_path)
+        concatenated = torch.cat((_img1, _img2, _img3, _p, _n), 0)    
+        imshow(torchvision.utils.make_grid(concatenated), 'Dissimilarity: positive distance {:.2f}; negative distance {:.2f}'.format(p_distance.cpu().data.numpy()[0][0], n_distance.cpu().data.numpy()[0][0]))
